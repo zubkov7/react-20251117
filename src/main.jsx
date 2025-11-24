8946fa837c74203c1ce354a829b2e133cdef2420
Zubkov Andrey <zubkov.andrey.99@mail.ru>
2025-11-24 14:28:00 +0300

    hw-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,31 @@
-import { createElement } from "react";
 import { createRoot } from "react-dom/client";
+import { headphones } from "./constants/mock";
 
 const root = document.getElementById("root");
 
 const reactRoot = createRoot(root);
 
-const items = [1, 2, 3, 4, 5];
-
-// reactRoot.render(
-//   createElement(
-//     "ul",
-//     {
-//       style: { color: "red" },
-//       className: "some-class another-class",
-//       onClick: () => {
-//         console.log("click");
-//       },
-//     },
-//     items.map((item) => createElement("li", {}, item))
-//   )
-// );
-
-console.log(reactRoot);
-
 reactRoot.render(
-  <ul className='some-class another-class' style={{ color: "red" }}>
-    {items.map((item) => (
-      <li key={item}>{item}</li>
-    ))}
-  </ul>
+  <div>
+    <h1>Headphones</h1>
+    <ol>
+      {headphones.map(({ name, id, reviews, codecs }) => (
+        <li key={id}>
+          <h2>{name}</h2>
+          <h3>Reviews</h3>
+          <ul>
+            {reviews.map((text) => (
+              <li key={text}>{text}</li>
+            ))}
+          </ul>
+          <h3>Codecs</h3>
+          <ul>
+            {codecs.map((codec) => (
+              <li key={codec}>{codec}</li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ol>
+  </div>
 );
